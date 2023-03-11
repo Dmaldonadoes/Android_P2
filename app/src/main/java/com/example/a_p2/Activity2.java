bd126d6fb44ac828e0aa07b644fba0b0aab6d836
Dmaldonadoes <dmaldonadoesteban@gmail.com>
2023-03-11 20:25:03 +0100

    La puta suma funciona en la segunda actividad la ostia

--- app/src/main/java/com/example/a_p2/Activity2.java
+++ app/src/main/java/com/example/a_p2/Activity2.java
@@ -2,13 +2,37 @@ package com.example.a_p2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Activity2 extends AppCompatActivity {
+    private TextView resu;
+    private EditText valor2;
+    private String valor1;
+    private int elputoprimernumerodemierda;
+    private int elputosegundonumerodemierda;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+        valor2 = (EditText) findViewById(R.id.val2texto);
+        Intent origen = getIntent();
+        valor1 = origen.getExtras().getString("val1");
+        elputoprimernumerodemierda = Integer.parseInt(valor1);
+
+        Log.d("123", "ksfbvpqiue " + elputoprimernumerodemierda);
+        resu = (TextView) findViewById(R.id.resultado);
+
+    }
+    public void suma2 (View quien){
+        elputosegundonumerodemierda = Integer.parseInt(String.valueOf(valor2.getText()));
+        int res = elputosegundonumerodemierda+elputoprimernumerodemierda;
+        Log.d("123", "suma2: " + res);
+        resu.setText(String.valueOf(res));
     }
 }
--- app/src/main/java/com/example/a_p2/MainActivity.java
+++ app/src/main/java/com/example/a_p2/MainActivity.java
@@ -5,16 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText valor1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        valor1 = (EditText) findViewById(R.id.val1texto);
     }
     public void cambiarActividad2 (View quien){
         Intent intencion = new Intent(this, Activity2.class);
         super.startActivity(intencion);
+    }
+    public void sumar (View quien){
+        Intent sumar1 = new Intent(this, Activity2.class);
+        sumar1.putExtra("val1", String.valueOf(valor1.getText()));
+        this.startActivity(sumar1);
     }
 }
